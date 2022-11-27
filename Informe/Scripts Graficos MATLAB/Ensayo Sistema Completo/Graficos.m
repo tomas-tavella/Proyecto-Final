a9c3c26ee822c208151ccfb111c9fa071f3a87b6
Tomás Tavella <tomas.tavella28@gmail.com>
2022-11-27 19:53:27 -0300

    (Informe) Empezar el capitulo final (ensayos)
    - Refinar el script de MATLAB para gráficos

--- Informe/Scripts Graficos MATLAB/Ensayo Sistema Completo/Graficos.m
+++ Informe/Scripts Graficos MATLAB/Ensayo Sistema Completo/Graficos.m
@@ -3,13 +3,14 @@
 %OUTPUT_90 = readtable('OUTPUT-90.CSV');
 %ConRc = importdata('ConRc.txt','\t');
 
-t1 = (readtable('F0027CH1.CSV').Var4 - min(readtable('F0027CH1.CSV').Var4))*10^6;
-t2 = (readtable('F0028CH1.CSV').Var4 - min(readtable('F0028CH1.CSV').Var4))*10^6;
+t1 = (readtable('7 - Vrect.CSV').Var4 - min(readtable('7 - Vrect.CSV').Var4))*10^6;
+t2 = (readtable('7 - Vp.CSV').Var4 - min(readtable('7 - Vp.CSV').Var4))*10^6;
 
 % DC 50% / RL 100ohm / Vin 30V
-Vrect = readtable('F0027CH1.CSV').Var5;     
-ILf = readtable('F0027CH2.CSV').Var5;
-Vp = readtable('F0028CH1.CSV').Var5;
+Vrect = readtable('7 - Vrect.CSV').Var5;     
+ILf = readtable('7 - ILf.CSV').Var5;
+Vp = readtable('7 - Vp.CSV').Var5;
+Iout = mean(ILf);
 
 set(0,'defaultTextInterpreter','none');
 set(groot,'defaultAxesTickLabelInterpreter','none'); 
@@ -17,7 +18,7 @@ set(groot,'defaultAxesTickLabelInterpreter','none');
 %% Fase 0° %%
 
 %subplot(2,1,1);
-set(gca, 'FontName', 'Montserrat');
+%set(gca, 'FontName', 'Montserrat');
 %t = title('Tensión rectificada y corriente de inductor');
 %t.FontSize = 20;
 yyaxis left;
@@ -26,6 +27,9 @@ ylabel('Tensión [V]', 'FontName', 'Montserrat SemiBold');
 axis([0 50 (min(Vrect)-0.1*(abs(max(Vrect)-min(Vrect)))) (max(Vrect)+0.1*(abs(max(Vrect)-min(Vrect))))]);
 yyaxis right;
 plot(t1, smoothdata(ILf,'gaussian',5), 'LineWidth', 1, 'Color','#A54100'); hold on; grid on;
+%yl=yline(Iout,':','LineWidth', 1.5,'Color','#A54100');
+f = gcf;
+f.Position = [100 100 600 240]      % Posición y tamaño de la ventana
 ax = gca;
 ax.FontName = 'Montserrat Medium';
 ylabel('Corriente [A]', 'FontName', 'Montserrat SemiBold');
